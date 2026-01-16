d091597d189f908362eda6cb871ed7eaa31a106f
Diganta <digantamodak1234@gmail.com>
2026-01-16 16:30:01 +0530

    p1v4 listing is done without image

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const express = require("express");
 const app = express()
 const mongoose = require("mongoose")
 const Listing = require("./models/listing.js");
+const path = require("path")
+app.set("view engine","ejs")
+app.set("views",path.join(__dirname,"views"))
+app.use(express.urlencoded ({ extended : true}))
 
 const mongo_url = "mongodb://127.0.0.1:27017/WonderLust2"
 main().then(()=>{
@@ -22,15 +26,20 @@ app.get("/",(req,res)=>{
     res.send("it is working")
 })
 
-app.get("/testListing",async (req,res)=>{
-    let sampleListing = new Listing({
-        title: "my villa",
-        description : " dscsds",
-        price : 1210,
-        country : "India"
-    })
-    await sampleListing.save()
-    console.log("sample saved")
-    res.send("sample saved successfully")
+app.get("/listings",async (req,res)=>{
+
+    const allListings = await Listing.find({})
+    res.render("listing/index.ejs",{allListings})
 
 })
+app.get("/listings/:id", async (req, res) => {
+    let { id } = req.params;
+    
+    const listingData = await Listing.findById(id);
+
+    if (!listingData) {
+        return res.status(404).send("Listing not found");
+    }
+
+    res.render("listing/show", { listingData });
+});
