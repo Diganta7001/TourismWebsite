370dcbb3ebfe24907ba04c44740d660d3d680287
Diganta <digantamodak1234@gmail.com>
2026-01-19 17:55:11 +0530

    few bugs occured due to adding boilerplate now they are fixed

--- app.js
+++ app.js
@@ -4,7 +4,11 @@ const mongoose = require("mongoose")
 const Listing = require("./models/listing.js");
 const path = require("path")
 const methodOverride = require("method-override")
+const ejsMate = require("ejs-mate")
 
+
+app.use(express.static(path.join(__dirname,"/public")))
+app.engine('ejs', ejsMate);
 app.set("view engine","ejs")
 app.set("views",path.join(__dirname,"views"))
 app.use(express.urlencoded ({ extended : true}))
