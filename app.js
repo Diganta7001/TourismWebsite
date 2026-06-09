3914a59ef32caa282ced3d221066b93acf224a98
Diganta <digantamodak1234@gmail.com>
2026-06-09 19:12:12 +0530

    Cookie in session option httpOnly: true

--- app.js
+++ app.js
@@ -29,6 +29,7 @@ const sessionConfig = {
     cookie:{
         expires: Date.now() + 1000 * 60 * 60 * 24 * 7, 
         maxAge: 1000 * 60 * 60 * 24 * 7, 
+        httpOnly: true
     }
 };
 app.use(session(sessionConfig));
