65db61772f7823ab53c4d32f6532994aec2d0af9
Diganta <digantamodak1234@gmail.com>
2026-06-19 18:59:43 +0530

    ph3PbVL geocoding is implemented with popup

--- public/js/map.js
+++ public/js/map.js
@@ -3,6 +3,18 @@ mapboxgl.accessToken = MAPBOX_ACCESS_TOKEN;
 const map = new mapboxgl.Map({
     accessToken: MAPBOX_ACCESS_TOKEN,
     container: 'map', // container ID
-    center: [77.209021, 28.613939], // starting position [lng, lat]. Note that lat must be set between -90 and 90
+    center: coordinates, // starting position [lng, lat]. Note that lat must be set between -90 and 90
     zoom: 9 // starting zoom
-    });
+});
+
+console.log(coordinates)
+ const marker1 = new mapboxgl.Marker({color:'red'})
+        .setLngLat(coordinates)
+        .setPopup(
+            new mapboxgl.Popup({ offset: 25 })
+            .setHTML(`
+                <h3>${listingLocation}</h3>
+                <p>Exact location will be shared after booking</p>
+            `)
+).addTo(map);
+
